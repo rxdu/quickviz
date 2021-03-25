00e65c8f35856cffd1414afac3d657fc8b32bd4c
Ruixiang Du <ruixiang.du@gmail.com>
2021-03-25 18:44:16 +0800

    saved work on plot buffer

---
+++ src/canvas/test/test_plot_buffer.cpp
@@ -0,0 +1,17 @@
+/*
+ * test_plot_buffer.cpp
+ *
+ * Created on: Mar 25, 2021 17:41
+ * Description:
+ *
+ * Copyright (c) 2021 Weston Robot Pte. Ltd.
+ */
+
+#include "canvas/plot_buffer.hpp"
+
+using namespace rdu;
+
+int main(int argc, char *argv[]) {
+  PlotBuffer buffer;
+  return 0;
+}
