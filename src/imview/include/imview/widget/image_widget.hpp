04386bea15fa8fe3dc827959332ee3ee2fc56ecb
Ruixiang Du <ruixiang.du@gmail.com>
2024-10-12 10:12:00 +0800

    image_widget: added resizing but not working yet

--- src/imview/include/imview/widget/image_widget.hpp
+++ src/imview/include/imview/widget/image_widget.hpp
@@ -38,6 +38,8 @@ class ImageWidget : public Panel {
 
   ~ImageWidget() { glDeleteTextures(1, &image_texture_); }
 
+  void SetKeepAspectRatio(bool keep) { keep_aspect_ratio_ = keep; }
+
   void Draw() override {
     Begin();
     {
@@ -49,10 +51,36 @@ class ImageWidget : public Panel {
       buffer_->Read(mat);
 
       if (!mat.empty()) {
-        //      cv::Mat display;
-        //      cv::resize(mat, display, cv::Size(width, height), 0, 0,
-        //      cv::INTER_LINEAR);
-        copy_texture_func_(mat, image_texture_);
+        if (keep_aspect_ratio_) {
+          cv::Mat proc = mat;
+          float aspect_ratio = mat.cols / (float)mat.rows;
+          float img_width = width;
+          float img_height = height;
+          if (width / height > aspect_ratio) {
+            img_width = height * aspect_ratio;
+          } else {
+            img_height = width / aspect_ratio;
+          }
+          if (img_width > width) {
+            // need to scale down
+            img_width = width;
+            img_height = width / aspect_ratio;
+          } else {
+            // need to scale down
+            img_height = height;
+            img_width = height * aspect_ratio;
+          }
+          cv::resize(mat, proc, cv::Size(img_width, img_height), 0, 0,
+                     cv::INTER_CUBIC);
+          // copy display to center of image
+          cv::Mat display = cv::Mat::zeros(height, width, mat.type());
+          cv::Rect roi((width - proc.cols) / 2, (height - proc.rows) / 2,
+                       proc.cols, proc.rows);
+          proc.copyTo(display(roi));
+          copy_texture_func_(display, image_texture_);
+        } else {
+          copy_texture_func_(mat, image_texture_);
+        }
       }
       ImGui::Image((void*)(intptr_t)image_texture_, ImVec2(width, height));
     }
@@ -63,6 +91,7 @@ class ImageWidget : public Panel {
   GLuint image_texture_;
   std::shared_ptr<BufferInterface<T>> buffer_;
   CopyTextureFunc copy_texture_func_;
+  bool keep_aspect_ratio_ = false;
 };
 }  // namespace quickviz
 
